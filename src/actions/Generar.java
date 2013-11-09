ca079cf7722819cd91e36393b2061ad52b9207eb
marcelonicolasgomezrivera <marcelogomez321@gmail>
2013-11-09 00:03:37 -0600

    Added actions.

--- src/actions/Generar.java
+++ src/actions/Generar.java
@@ -1,24 +1,62 @@
 package actions;
 
-import model.DBManager;
+import model.*;
 
 public class Generar {
     private DBManager dbManager = new DBManager();
+    private ReporteAnalisisPaciente[]  reportesAnalisisPaciente;
+    private ReporteDoctoresPaciente[]  reportesDoctoresPaciente;
+    private ReportePacientesAnalisis[] reportesPacientesAnalisis;
+    private ReportePacientesDoctor[]   reportesPacientesDoctor;
+    String claveDoctor;
+    String  claveAnalisis;
+    String  clavePaciente;
 
     public String reporteAnalisisPaciente() throws Exception{
+        reportesAnalisisPaciente = dbManager.generarReporteAnalisisPaciente(clavePaciente);
         return "success";
     }
 
     public String reportePacientesAnalisis() throws Exception{
+        reportesPacientesAnalisis = dbManager.generarReportePacientesAnalisis(claveAnalisis);
         return "success";
     }
 
     public String reporteDoctoresPaciente() throws Exception{
+        reportesDoctoresPaciente = dbManager.generarReporteDoctoresPaciente(clavePaciente);
         return "success";
     }
 
     public String reportePacientesDoctor() throws Exception{
+        reportesPacientesDoctor  = dbManager.generarReportePacientesDoctor(claveDoctor);
         return "success";
     }
 
+    public ReportePacientesDoctor[] getReportesPacientesDoctor() {
+        return reportesPacientesDoctor;
+    }
+
+    public ReportePacientesAnalisis[] getReportesPacientesAnalisis() {
+        return reportesPacientesAnalisis;
+    }
+
+    public ReporteDoctoresPaciente[] getReportesDoctoresPaciente() {
+        return reportesDoctoresPaciente;
+    }
+
+    public ReporteAnalisisPaciente[] getReportesAnalisisPaciente() {
+        return reportesAnalisisPaciente;
+    }
+
+    public void setClavePaciente(String clavePaciente) {
+        this.clavePaciente = clavePaciente;
+    }
+
+    public void setClaveAnalisis(String claveAnalisis) {
+        this.claveAnalisis = claveAnalisis;
+    }
+
+    public void setClaveDoctor(String claveDoctor) {
+        this.claveDoctor = claveDoctor;
+    }
 }
