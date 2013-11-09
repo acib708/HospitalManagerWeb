8e50449e27ef4ff3eb08aa250ac4c8053af67e2f
Alejandro Cardenas <acib708@gmail.com>
2013-11-09 00:36:06 -0600

    Finished Action: borrar.

--- src/actions/Borrar.java
+++ src/actions/Borrar.java
@@ -4,16 +4,31 @@ import model.DBManager;
 
 public class Borrar {
     private DBManager dbManager = new DBManager();
+    //Keys needed to erase each entry in the DB
+    private String  clave;
+    private Boolean deleted;
 
     public String doctor() throws Exception{
+        deleted = dbManager.borrarDoctor(clave);
         return "success";
     }
 
     public String paciente() throws Exception{
+        deleted = dbManager.borrarPaciente(clave);
         return "success";
     }
 
     public String analisis() throws Exception{
+        deleted = dbManager.borrarAnalisis(clave);
         return "success";
+    }
+
+    /* Getters and Setters */
+    public Boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
     }
 }
