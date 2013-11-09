f9af9dad3b1898613fc8d9d1c9e51e0cd678eec7
marcelonicolasgomezrivera <marcelogomez321@gmail>
2013-11-09 00:36:42 -0600

    Completed Action Actualizar

--- src/actions/Actualizar.java
+++ src/actions/Actualizar.java
@@ -9,61 +9,61 @@ public class Actualizar {
     private DBManager dbManager = new DBManager();
     private Boolean   response;
 
-    private String          claveDoctor,nombreDoctor,direccionDoctor,especialidadDoctor,telefonoDoctor,fotoDoctor;
-    private String          clavePaciente,direccionPaciente,fotoPaciente,nombrePaciente,telefonoPaciente;
-    private String          claveAnalisis;
+    private String          clave,nombre,direccion,especialidad,telefono,foto,tipo,descripcion;
     private Doctor          doctor;
     private Paciente        paciente;
     private AnalisisClinico analisis;
 
     public String doctor() throws Exception{
-        getDataDoctor();
+        doctor = new Doctor();
+        doctor.setClave(clave);
+        doctor.setDireccion(direccion);
+        doctor.setEspecialidad(especialidad);
+        doctor.setFoto(foto);
+        doctor.setNombre(nombre);
+        doctor.setTelefono(telefono);
         response = dbManager.actualizarDoctor(doctor);
         return "success";
     }
 
     public String paciente() throws Exception{
-        getDataPaciente();
+        paciente = new Paciente();
+        paciente.setClave(clave);
+        paciente.setDireccion(direccion);
+        paciente.setFoto(foto);
+        paciente.setNombre(nombre);
+        paciente.setTelefono(telefono);
         response = dbManager.actualizarPaciente(paciente);
         return "success";
     }
 
     public String analisis() throws Exception{
-        getDataAnalisis();
+        analisis = new AnalisisClinico();
+        analisis.setClave(clave);
+        analisis.setDescripcion(descripcion);
+        analisis.setTipo(tipo);
         response = dbManager.actualizarAnalaisis(analisis);
         return "success";
     }
-    //Getters for attributes
+
     public String getDataDoctor(){
-        doctor = dbManager.consultarDoctorClave(claveDoctor);
+        doctor = dbManager.consultarDoctorClave(clave);
         return "success";
     }
 
     public String getDataPaciente(){
-        paciente = dbManager.consultarPacienteClave(clavePaciente);
+        paciente = dbManager.consultarPacienteClave(clave);
         return "success";
     }
 
     public String getDataAnalisis(){
-        analisis = dbManager.consultarAnalisisClave(claveAnalisis);
+        analisis = dbManager.consultarAnalisisClave(clave);
         return "success";
     }
 
-    //Setters for parameters
-    public void setClaveDoctor(String claveDoctor) {
-        this.claveDoctor = claveDoctor;
-    }
-
-    public void setClavePaciente(String clavePaciente) {
-        this.clavePaciente = clavePaciente;
-    }
-
-    public void setClaveAnalisis(String claveAnalisis) {
-        this.claveAnalisis = claveAnalisis;
-    }
-
-    public Doctor getDoctor() {
-        return doctor;
+    //Getters for attributes
+    public Boolean getResponse() {
+        return response;
     }
 
     public Paciente getPaciente() {
@@ -72,5 +72,42 @@ public class Actualizar {
 
     public AnalisisClinico getAnalisis() {
         return analisis;
+    }
+
+    //Setters for parameters
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
     }
 }
